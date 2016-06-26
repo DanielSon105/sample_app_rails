4af1123017c7d25bfff28117b35d28b747f1f1ac
Michael Voropaiev <m.voropaiev@gmail.com>
2016-06-27 00:07:59 +0300

    update logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  end
 end
